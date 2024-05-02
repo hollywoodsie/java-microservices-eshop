02c42f02d8a16d3646c61ae4530600fdf8d93cfa
hollywoodsie <kovalioff28@gmail.com>
2024-05-02 17:18:58 +0300

    feat : jwt filter for spring security

--- api-gateway/src/main/java/com/eshop/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/eshop/apigateway/ApiGatewayApplication.java
@@ -1,20 +1,22 @@
 package com.eshop.apigateway;
 
 
+import com.eshop.apigateway.filter.JwtFilter;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @SpringBootApplication
@@ -25,8 +27,13 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+
+    private JwtFilter jwtFilter;
+
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges ->
@@ -36,8 +43,8 @@ public class ApiGatewayApplication {
                                 .pathMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN")
                                 .pathMatchers(HttpMethod.PUT, "/products/**").hasRole("ADMIN")
                                 .pathMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
-                                .pathMatchers(HttpMethod.POST, "/users/**").hasAnyRole("USER","ADMIN") //temporary
-                                .pathMatchers(HttpMethod.GET, "users/**").hasAnyRole("USER","ADMIN")
+                                .pathMatchers(HttpMethod.POST, "/users/**").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/users/**").hasAnyRole("USER","ADMIN")
                                 .pathMatchers(HttpMethod.DELETE, "users/**").hasRole("ADMIN")
                                 .pathMatchers(HttpMethod.GET, "/cart/**").hasAnyRole("USER","ADMIN")
                                 .pathMatchers(HttpMethod.POST, "/cart/**").hasAnyRole("USER","ADMIN")
@@ -46,30 +53,7 @@ public class ApiGatewayApplication {
 
                                 .anyExchange().authenticated()
                 )
-                .httpBasic(withDefaults())
+                .addFilterBefore(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
     }
-
-    @Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return DigestUtils.sha256Hex(rawPassword.toString());
-            }
-
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return encode(rawPassword).equals(encodedPassword);
-            }
-
-        };
-    }
-
 }
---
+++ api-gateway/src/main/java/com/eshop/apigateway/filter/JwtFilter.java
@@ -0,0 +1,100 @@
+package com.eshop.apigateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Component
+public class JwtFilter implements WebFilter {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    private Key key;
+
+    @PostConstruct
+    public void initKey() {
+        this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        String token = headers.getFirst("Authorization");
+
+        if (token != null && token.startsWith("Bearer ")) {
+            String jwtToken = token.substring(7);
+            String userId = extractUserIdFrom(jwtToken);
+            exchange.getRequest().mutate().headers(httpHeaders -> {
+                if (!httpHeaders.containsKey("X-UserId")) {
+                    httpHeaders.add("X-UserId", userId);
+                }
+            });
+
+            UsernamePasswordAuthenticationToken authenticationToken = validateToken(jwtToken);
+
+            if (authenticationToken != null && authenticationToken.isAuthenticated()) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                context.setAuthentication(authenticationToken);
+                exchange.getAttributes().put(SecurityContext.class.getName(), context);
+
+                return chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(authenticationToken));
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+    private UsernamePasswordAuthenticationToken validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken);
+
+            Date expiration = claims.getBody().get("exp", Date.class);
+
+            if (expiration != null && expiration.before(new Date())) {
+                throw new RuntimeException("Token is expired");
+            }
+
+            String userId = claims.getBody().get("id", String.class);
+            String role = claims.getBody().get("role", String.class);
+
+            if (userId != null && role != null) {
+                List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+
+                return new UsernamePasswordAuthenticationToken(userId, null, authorities);
+            }
+        } catch (Exception e) {
+            System.out.println(e); //throw exceptions
+        }
+        return null;
+    }
+
+    private String extractUserIdFrom(String jwtToken) {
+        Jws<Claims> claims = Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken);
+
+        return claims.getBody().get("id", String.class);
+    }
+}
